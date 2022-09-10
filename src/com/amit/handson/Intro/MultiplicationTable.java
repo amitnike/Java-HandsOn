daaa1f331747955170fa19687f0dab611d211472
Amit <amit.naikwadi84@gmail.com>
2022-09-10 23:44:56 +0530

    Table class added

---
+++ src/com/amit/handson/Intro/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package com.amit.handson.Intro;
+
+//For a given number A, print its multiplication table having the first 10 multiples.
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        printTable(input);
+    }
+
+    private static void printTable(int input) {
+        for (int i=1;i<=10;i++){
+            System.out.println(input + " * "+ i + " = "+input*i);
+        }
+    }
+}
