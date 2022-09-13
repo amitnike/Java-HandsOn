587dc739e44fb5fcc7af4bcad3d9ffc56b1c8e2d
Amit <amit.naikwadi84@gmail.com>
2022-09-13 18:13:55 +0530

    question description added

--- src/com/amit/handson/twoPointer/ThreeSumZero.java
+++ src/com/amit/handson/twoPointer/ThreeSumZero.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+//Given an array A of N integers, are there elements a, b, c in S such that a + b + c = 0
+//
+//Find all unique triplets in the array which gives the sum of zero.
+//
+//Note:
+//
+//Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c) The solution set must not contain duplicate triplets.
 public class ThreeSumZero {
 
     public static  void main(String[] args){
